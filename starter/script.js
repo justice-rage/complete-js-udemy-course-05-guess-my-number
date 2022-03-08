141e2d7eabc4cea8900f2c1edb22038816acbfc8
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-03-08 08:39:03 -0800

    complete lecture 74

--- starter/script.js
+++ starter/script.js
@@ -25,14 +25,23 @@ document.querySelector('.check').addEventListener('click', function() {
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🥳 Correct number!';
   } else if (guess > secretNumber) {
-    document.querySelector('.message').
-    textContent = '📈 To high...';
-    score--;
-    document.querySelector('.score').textContent = score;
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 To high...';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '😶 You lose.';
+      document.querySelector('.score').textContent = 0;
+    }
   } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = '📉 To low...';
-    score--;
-    document.querySelector('.score').textContent = score;
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 To low...';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '😶 You lose.';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
 
