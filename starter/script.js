d3415061241fd7f85403d57870c89043b960d6e0
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-03-07 18:02:55 -0800

    work in progress - lecture 74

--- starter/script.js
+++ starter/script.js
@@ -11,11 +11,29 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function() {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
   if (!guess) {
     document.querySelector('.message').textContent = '☠️ No number!'
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🥳 Correct number!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').
+    textContent = '📈 To high...';
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = '📉 To low...';
+    score--;
+    document.querySelector('.score').textContent = score;
   }
 });
+
+// document.querySelector('.score').textContent = score;
